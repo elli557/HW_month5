10405807f6393f10fc8b7772b05f2095164e3fc9
emwauqz <emwauqz@gmail.com>
2025-06-12 11:56:00 +0600

    HW_2

--- product/serializers.py
+++ product/serializers.py
@@ -8,15 +8,18 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 class ProductReviewSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
+    rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'price', 'reviews']
-  #  def get_rating(self, obj):
-        #reviews = obj.reviews.all()
-   #     if reviews.exists():
-       #     return round(sum([r.stars for r in reviews]) / reviews.count(), 2)
-     #   return None
+        fields = ['id', 'title', 'description', 'price', 'reviews', 'rating']
+
+
+    def get_rating(self, obj):
+        reviews = obj.reviews.all()
+        if reviews.exists():
+            return round(sum([r.stars for r in reviews]) / reviews.count(), 2)
+        return None
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
